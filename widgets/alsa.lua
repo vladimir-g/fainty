e1334336300973cfacd1e264fc0fe5031c4b99b3
Vladimir Gorbunov <vsg@suburban.me>
2015-04-10 22:34:03 +0300

    alsa: change constructor arguments and add step

--- widgets/alsa.lua
+++ widgets/alsa.lua
@@ -13,7 +13,7 @@ local pairs = pairs
 local timer = timer
 local type = type
 
--- Alsa controller class
+-- Channel object, represents one channel of sound device
 local AlsaChannel = {}
 
 function AlsaChannel:new(cardid, channel)
@@ -25,7 +25,7 @@ end
 -- Get card volume and muted state
 function AlsaChannel:get_data()
    local data = {}
-   local fd = io.popen("amixer -c " .. self.cardid .. 
+   local fd = io.popen("amixer -c " .. self.cardid ..
                        " -- sget " .. self.channel)
    if not fd then return end
    local result = fd:read("*all")
@@ -52,7 +52,7 @@ end
 -- Raise card volume
 function AlsaChannel:raise(value)
    value = value or 1
-   awful.util.spawn_with_shell("amixer -q -c " .. self.cardid .. 
+   awful.util.spawn_with_shell("amixer -q -c " .. self.cardid ..
                                " -- sset " .. self.channel
                                .. " " .. value .. "+", false)
 end
@@ -60,14 +60,14 @@ end
 -- Lower card volume
 function AlsaChannel:lower(value)
    value = value or 1
-   awful.util.spawn_with_shell("amixer -q -c " .. self.cardid .. 
+   awful.util.spawn_with_shell("amixer -q -c " .. self.cardid ..
                                " -- sset " .. self.channel
                                .. " " .. value .. "-", false)
 end
 
 -- Mute or unmute card
 function AlsaChannel:toggle()
-   awful.util.spawn("amixer -q -c " .. self.cardid .. 
+   awful.util.spawn("amixer -q -c " .. self.cardid ..
                     " sset " .. self.channel .. " toggle", false)
 end
 
@@ -91,16 +91,18 @@ function AlsaWidget:refresh(card_num)
    local label = ''
    if not data then
       self:set_markup(self.error_msg)
-      naughty.notify({ preset = naughty.config.presets.critical,
-                       title = "Error in alsa widget",
-                       text = "Error while trying to run amixer" })
+      if self.notify_errors then
+         naughty.notify({ preset = naughty.config.presets.critical,
+                          title = "Error in alsa widget",
+                          text = "Error while trying to run amixer" })
+      end
       return
    end
    if data.mute then
-      label = '<span color="' .. self.color.muted .. '">' 
+      label = '<span color="' .. self.color.muted .. '">'
          .. card.label .. '</span>'
    else
-      label = '<span color="' .. self.color.unmuted .. '">' 
+      label = '<span color="' .. self.color.unmuted .. '">'
          .. card.label .. '</span>'
    end
    self:set_markup(self.format:format(label, data.volume))
@@ -108,16 +110,16 @@ end
 
 -- Raise volume
 function AlsaWidget:raise(value, card_num)
-   value = value or 1
    local card = self:select_card(card_num)
+   value = value or card.step
    card.control:raise(value)
    self:refresh()
 end
 
 -- Lower volume
 function AlsaWidget:lower(value, card_num)
-   value = value or 1
    local card = self:select_card(card_num)
+   value = value or card.step
    card.control:lower(value)
    self:refresh()
 end
@@ -139,68 +141,81 @@ end
 function AlsaWidget:get_menu_items()
    local card_list = {}
    for k, v in pairs(self.cards) do
-      card_list[k] = { v.label .. " " .. v.name, 
+      card_list[k] = { v.label .. " " .. v.name,
                        function ()
-                          self.selected = self.cards[k] 
+                          self.selected = self.cards[k]
                           self:refresh()
                        end }
    end
    return card_list
 end
 
--- Create widget. Look to widgets module readme file for more info.
-local function new(card_list, settings)
-   if not card_list then
-      return nil
+-- Create widget
+local function new(args)
+   if not args.settings then args.settings = {} end
+
+   if not args.card_list then
+      -- Set default card
+      args.card_list = {
+         {label = "♪", cardid = 0, channel = "Master", name = "Default"}
+      }
    end
+
    -- Create widget
-   settings = settings or {}
+   settings = args.settings or {}
    local obj = wibox.widget.textbox()
-   -- Put AlsaWidet's methods to textbox, some kind of multiple inheritance
+
    for k, v in pairs(AlsaWidget) do
       if type(v) == "function" then
          obj[k] = v
       end
    end
+
    -- Set settings
    obj.cards = {}
-   obj.format = settings.format or "%s% 3d%%"
+   obj.format = args.settings.format or "%s% 3d%%"
    obj.color = {
-      muted = settings.color_muted or "#F80000", -- Red
-      unmuted = settings.color_unmuted or "#00EE00"  -- Green
+      muted = args.settings.color_muted or "#F80000", -- Red
+      unmuted = args.settings.color_unmuted or "#00EE00"  -- Green
    }
-   obj.error_msg = settings.error_msg or '<span color="#FF0004">#</span>'
-   menu_theme = settings.menu_theme or { width = 120, height = 15 }
+   obj.error_msg = args.settings.error_msg or '<span color="#FF0004">#</span>'
+   obj.notify_errors = args.settings.notify_errors or true
+   menu_theme = args.settings.menu_theme or { width = 120, height = 15 }
+   obj.step = args.settings.step or 1
    -- Create card list
-   for k, v in pairs(card_list) do
-      obj.cards[k] = { 
+   for k, v in pairs(args.card_list) do
+      obj.cards[k] = {
          label = v.label,
          name = v.name or "",
          control = AlsaChannel:new(v.cardid, v.channel),
+         step = v.step or obj.step,
          selected = false
       }
    end
    obj.selected = obj.cards[1]
    obj:refresh()
+
    -- Add menu
    local card_menu = awful.menu({ items = obj:get_menu_items(),
                                   theme = menu_theme })
-   -- Bind buttons
-   if not settings.dont_bind_buttons then
+
+   -- Bind buttons if needed
+   if not args.settings.dont_bind_buttons then
       obj:buttons(
          awful.util.table.join(
             awful.button({ }, 1, function () obj:toggle() end),
             awful.button({ }, 3, function () card_menu:toggle() end),
             awful.button({ }, 4, function () obj:raise() end),
             awful.button({ }, 5, function () obj:lower() end),
-            awful.button({ "Shift" }, 4, function () obj:raise(10) end),
-            awful.button({ "Shift" }, 5, function () obj:lower(10) end),
-            awful.button({ "Control" }, 4, function () obj:raise(5) end),
-            awful.button({ "Control" }, 5, function () obj:lower(5) end)
-                              ))
+            awful.button({ "Shift" }, 4, function () obj:raise(obj.step * 10) end),
+            awful.button({ "Shift" }, 5, function () obj:lower(obj.step * 10) end),
+            awful.button({ "Control" }, 4, function () obj:raise(obj.step * 5) end),
+            awful.button({ "Control" }, 5, function () obj:lower(obj.step * 5) end)
+      ))
    end
-   -- Set update timer
-   refresh_timer = timer({ timeout = settings.refresh_timeout or 10 })
+
+   -- Init update timer
+   refresh_timer = timer({ timeout = args.settings.refresh_timeout or 10 })
    refresh_timer:connect_signal("timeout", function() obj:refresh() end)
    refresh_timer:start()
    return obj
