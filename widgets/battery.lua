0d9025bcae8d2eae374516edcd7ac27cfc742ba6
Vladimir Gorbunov <vsg@suburban.me>
2015-04-19 22:29:36 +0300

    Add battery widget

---
+++ widgets/battery.lua
@@ -0,0 +1,255 @@
+--------------------
+-- Battery widget --
+--------------------
+-- Copyright (c) 2013-2015 Vladimir Gorbunov
+-- Release under MIT license, see LICENSE file for more details
+
+local wibox = require("wibox")
+local awful = require('awful')
+local naughty = require('naughty')
+local math = math
+local setmetatable = setmetatable
+local io = io
+local os = os
+local pairs = pairs
+local table = table
+
+-- STRING INTERPOLATION FIXME
+function interp(s, tab)
+   return (
+      s:gsub('%%%((%a%w*)%)([-0-9%.]*[cdeEfgGiouxXsq])',
+             function(k, fmt) return tab[k] and ("%"..fmt):format(tab[k]) or
+                '%('..k..')'..fmt end))
+end
+getmetatable("").__mod = interp
+
+local Battery = { mt = {} }
+
+Battery.path = '/sys/class/power_supply/'
+
+function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end                             -- FIXME
+
+function Battery:new(bat)
+   local new_instance = { name = bat, 
+                          path = Battery.path .. bat .. '/' }
+   setmetatable(new_instance, { __index = Battery })
+   return new_instance
+end
+
+function Battery:read(val)
+   local path = self.path .. val
+   local fd = io.open(path, 'r')
+   if not fd then
+      return nil
+   end
+   data = fd:read('*all')
+   fd:close()
+   return trim(data)
+end
+
+function Battery:is_present()
+   local present = self:read('present')
+   if present == '1' then
+      return true
+   else
+      return false
+   end
+end
+
+function Battery:status()
+   local data = { status = self:read('status'),
+                  percent = nil,
+                  time = nil,
+                  capacity = nil,
+                  watt = nil }
+
+   local current, full, rate
+   data.percent = tonumber(self:read('capacity'))
+   -- local rate_v = tonumber(self:read('voltage_now'))
+
+   if self:read('energy_now') then
+      current = tonumber(self:read('energy_now'))
+      full = tonumber(self:read('energy_full'))
+      rate = tonumber(self:read('power_now'))
+   elseif self:read('charge_now') then
+      current = tonumber(self:read('charge_now'))
+      full = tonumber(self:read('charge_full'))
+      rate = tonumber(self:read('current_now'))
+   else
+      return data
+   end
+
+   -- if rate ~= nil and rate_v ~= nil then
+   --    data.watt = (rate * rate_v) / 1e12
+   -- end
+
+   if not data.percent then
+      data.percent = math.min(math.floor(current / full * 100), 100)
+   end
+
+   local left
+   if rate ~= 0 and data.status == 'Discharging' then
+      left = current / rate
+   elseif rate ~= 0 and data.status == 'Charging' then
+      left = (full - current) / rate
+   else
+      left = nil
+   end 
+
+   if left then
+      data.hours = math.floor(left)
+      data.minutes = math.floor(60 * (left - data.hours))
+      data.in_seconds = math.floor(data.hours * 3600 +
+                                      (data.minutes * 60))
+   else
+      data.hours = nil
+      data.minutes = nil
+      data.in_seconds = nil
+   end
+
+   return data
+end      
+
+local BatteryWidget = { mt = {} }
+
+function BatteryWidget:refresh()
+   local bat = self.selected
+   if bat == nil then
+      self:set_markup(self.settings.fmt_not_present)
+      return
+   end
+
+   if bat:is_present() then
+      local data = bat:status()
+      local fmt = self.settings.fmt_unknown
+      if data.status == 'Discharging' then
+         fmt = self.settings.fmt_discharging
+      elseif data.status == 'Charging' then
+         fmt = self.settings.fmt_charging
+      end
+
+      if (data.in_seconds and data.in_seconds < self.warning_seconds
+          and data.status == 'Discharging') then
+         fmt = self.settings.fmt_warning
+         if self.settings.notify_warning then
+            self.notif_id = naughty.notify({
+                  preset = naughty.config.presets.critical,
+                  timeout = 10,
+                  replaces_id = self.notif_id or nil,
+                  title = self.settings.notify_warning_title,
+                  text = self.settings.notify_warning_text
+            }).id
+         end
+      end
+
+      local percent = 0
+      if data.percent then
+         percent = data.percent
+      end
+
+      self:set_markup(fmt % {
+                         percent = percent or 0,
+                         hours = data.hours or 0,
+                         minutes = data.minutes or 0,
+                         in_seconds = data.in_seconds or 0,
+                         watt = data.watt or 0
+      })
+   else
+      self:set_markup(self.settings.fmt_not_present)
+   end
+   return
+end
+
+-- Get batteries table for awful.menu
+function BatteryWidget:get_menu_items()
+   local battery_list = {}
+   for k, v in pairs(self.batteries) do
+      battery_list[k] = {
+         v.name,
+         function ()
+            self.selected = self.batteries[k]
+            self:refresh()
+         end
+      }
+   end
+   return battery_list
+end
+
+local function new(args)
+   -- TODO: configurable popup with battery info
+   -- TODO: Additional battery params, voltage, etc
+   args.selected = args.selected or 'BAT0'
+   args.settings = args.settings or {}
+   settings = {
+      fmt_discharging = args.settings.fmt_discharging or ' <span color="#F80000">↯</span>%(percent)3d% (%(hours)d:%(minutes)02d)', -- Red color
+      fmt_charging = args.settings.fmt_charging or ' <span color="#00EE00">↯</span>%(percent)3d% (%(hours)d:%(minutes)02d)', -- Green color
+      fmt_unknown = args.settings.fmt_unknown or ' <span color="#FFFFFF">↯</span>%(percent)3d%', -- White color
+      fmt_warning = args.settings.fmt_warning or ' <span color="#F80000">↯!%(percent)3d%</span>', -- Full red
+      fmt_not_present = args.settings.fmt_not_present or ' <span color="#FFFFFF">↯</span> N/A', -- White color
+      menu_theme = args.settings.menu_theme or { width = 120, height = 15 },
+      bind_buttons = args.settings.bind_buttons or true,
+      warning_seconds = args.settings.warning_seconds or 600,
+      refresh_timeout = args.settings.refresh_timeout or 25,
+      notify_warning = args.settings.notify_warning or true,
+      notify_warning_title = args.settings.notify_warning_title or
+         'Low battery',
+      notify_warning_text = args.settings.notify_warning_text or
+         'Less than 10 minutes remaining'
+   }
+
+   local obj = wibox.widget.textbox()
+   for k, v in pairs(BatteryWidget) do
+      obj[k] = v
+   end
+   obj.settings = settings
+   obj.warning_seconds = settings.warning_seconds
+   -- Get battery list
+   obj.batteries = {}
+   obj.selected = nil
+   local batlist = io.popen('ls ' .. Battery.path)
+   for line in batlist:lines() do
+      if line:match('^BAT%d+') then
+         battery = Battery:new(line)
+         if battery.name == args.selected then
+            obj.selected = battery
+         end
+         table.insert(obj.batteries, battery)
+      end
+   end
+   if not obj.selected and #obj.batteries ~= 0 then
+      obj.selected = obj.batteries[1]
+   end
+
+   batlist:close()
+
+   -- Add menu if there is more than one battery\
+   if #obj.batteries ~= 1 then
+      local battery_menu = awful.menu({ items = obj:get_menu_items(),
+                                        theme = settings.menu_theme })
+      if settings.bind_buttons then
+         obj:buttons(
+            awful.util.table.join(
+               awful.button({ }, 3, function () battery_menu:toggle() end)
+         ))
+      end
+   end
+
+   obj:refresh()
+
+   -- Set update timer
+   if #obj.batteries ~= 0 then
+      refresh_timer = timer({ timeout = settings.refresh_timeout })
+      refresh_timer:connect_signal("timeout", function() obj:refresh() end)
+      refresh_timer:start()
+   end
+
+   return obj
+end
+
+function BatteryWidget.mt:__call(...)
+   return new(...)
+end
+
+return setmetatable(BatteryWidget, BatteryWidget.mt)
